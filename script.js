1e8cf51691a341edb0066f5020e95e991f854ce3
Sinkevich Ilona <Sinkevich.Ilona@gmail.com>
2020-11-02 14:17:57 +0300

    fix: fixed newElem.style | script.js

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 'use strict';
 
 const DomElement = function() {
-    this.height = '';
-    this.width ='';
-    this.bg = '';
-    this.fontSize = '';
-    this.selector = '';
+    this.height = '500px';
+    this.width ='500px';
+    this.bg = 'blue';
+    this.fontSize = '120px';
+    this.selector = '#lalala';
     this.creator = function() {
 
         if (this.selector[0] === ".") {
           let className = this.selector.slice(1);
           const newElem = document.createElement('div');
           newElem.classList.add('className');
+          newElem.style.cssText = `width: ${this.width};
+                            height: ${this.height}; 
+                            background-color: ${this.bg};
+                            font-size: ${this.fontSize};
+                          `;
           newElem.textContent = 'hi';
           document.body.append(newElem);
         };
@@ -20,6 +25,11 @@ const DomElement = function() {
           let idName = this.selector.slice(1);
           const newElem = document.createElement('p');
           newElem.setAttribute('id', idName);
+          newElem.style.cssText = `width: ${this.width};
+                            height: ${this.height}; 
+                            background-color: ${this.bg};
+                            font-size: ${this.fontSize};
+                          `;
           newElem.textContent = 'hi-hi';
           document.body.append(newElem);
         };        
@@ -28,12 +38,6 @@ const DomElement = function() {
 
 
 const domElement = new DomElement;
-
-domElement.height = '1000px';
-domElement.width = '1000px';
-domElement.bg = 'blue';
-domElement.fontSize = '120px';
-domElement.selector = '#lalala';
 
 domElement.creator();
 
